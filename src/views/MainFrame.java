1fe270289598bd1967b1ca09a7ac7791221a51d1
Miloš Zeljko <zeljko.ra24.2019@uns.ac.rs>
2022-01-20 16:31:58 +0100

    Fixed save on exit

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -3,12 +3,20 @@
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
+
+import methods.WriterReader;
+import models.DbSubjects;
+import models.Subject;
 
 public class MainFrame extends JFrame{
 	
@@ -33,7 +41,57 @@ public class MainFrame extends JFrame{
 		setSize(width*3/4, height*3/4);
 		setLocationRelativeTo(null);
 		setTitle(resourceBundle.getString("mainFrameTitle"));
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowListener() {
+			
+			@Override
+			public void windowOpened(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowIconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeiconified(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowDeactivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+				int userInput = JOptionPane.showConfirmDialog(MainFrame.getInstance(), "Da li želite da saèuvate izmene pre zatvaranja?",
+						"Èuvanje izmena", JOptionPane.YES_NO_OPTION);
+				if (userInput == JOptionPane.YES_OPTION) {
+					WriterReader.getInstance().SaveData();
+					}
+				MainFrame.getInstance().dispose();
+			}
+			
+			@Override
+			public void windowClosed(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void windowActivated(WindowEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 		Image icon = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB_PRE);
 		setIconImage(icon);
 		}
